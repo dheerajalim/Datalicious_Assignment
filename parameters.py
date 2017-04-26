3200875c6d33ece0da5270bfb7632abf665be174
dheerajalim <dheeraj.alim@gmail.com>
2017-04-26 23:55:20 +0530

    Parameter List
    This file contains all the parameters used in script.
    
    Change as per your need

---
+++ parameters.py
@@ -0,0 +1,28 @@
+# Chrome Driver Executable file path
+
+CHROME_EXE_PATH = "C:\Personal\Development\Python\seliniumtest\chromedriver.exe"
+
+
+# Browsermob proxy server .bat file location
+
+BROWSERMOB_SERVER_PATH = r"C:\Personal\Development\Data_test\browsermob-proxy-bat\bin\browsermob-proxy"
+
+# Setting the Search Process
+
+WEBSITE_URL = "http://www.google.com"
+WEBSITE_SEARCH_TERM = "Datalicious"
+
+# Setting request Parameter
+
+GOOGLE_ANALYTICS_REQUEST ="https://www.google-analytics.com/r/collect"
+OPTIMAHUB_REQUEST = "dc.optimahub.com"
+
+# Setting Filename
+
+HAR_JSON_FILE = 'Datalicious_HAR.json'
+CSV_FILE_NAME = 'CSV_LOGS.csv'
+
+# Setting Google Analytics Parameters
+
+GA_DT_PARA = "dt"
+GA_DP_PARA = "dp"
